c9b0fab0d71ef125943057d4082213a921200c24
Charlie Meyer <charlie.g.meyer@icloud.com>
2024-01-25 14:35:29 -0500

    added a button

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  
   return (
     <View style={styles.container}>
       <Text>Hello ayaan</Text>
       <StatusBar style="auto" />
+      <button>Click me</button>
     </View>
   );
 }
