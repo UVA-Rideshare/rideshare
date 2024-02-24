5cc55a656d0405df3a8b8dc0dcd3e27413cc2976
unknown <ayaanrah99@gmail.com>
2024-02-24 16:26:33 -0500

    added navigation for createPost page

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './app/index.js';
 import SignUpScreen from './app/signup.js';
 import Homepage from './app/homepage.js'
+import CreatePost from './app/createPost.js';
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,7 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="Homepage" component={Homepage} />
+        <Stack.Screen name= "CreatePost" component={CreatePost} />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ app/createPost.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const CreatePost = ({ navigation }) => {
+
+}
+
+export default CreatePost;
