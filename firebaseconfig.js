1909e35830132b259e3a686e4caf4e1658202a87
unknown <ayaanrah99@gmail.com>
2024-03-28 23:37:05 -0400

    added firebase config stuff

---
+++ firebaseconfig.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5Q-r2nWv99-axtfUoM5xlvCZ9DHUOE_4",
+  authDomain: "uva-rideshare.firebaseapp.com",
+  projectId: "uva-rideshare",
+  storageBucket: "uva-rideshare.appspot.com",
+  messagingSenderId: "3013726099",
+  appId: "1:3013726099:web:3bb82484fa405a99712266",
+  measurementId: "G-HDDRT3Y7NX"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
