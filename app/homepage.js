2c0f69cbf66aa81aff1d3d50fa34d59b0c339bae
Arthur Wu <arthur.kainan.wu@gmail.com>
2024-04-20 18:18:43 -0400

    test branch works with bad UI

--- app/homepage.js
+++ app/homepage.js
@@ -1,52 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import { View, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import HomepagePost from './components/homepagePost.js'
+import HomepagePost from './components/homepagePost.js';
 import CreatePost from './createPost.js';
 import BottomNavBar from './components/navbar.js';
 
 import { collection, getDocs } from 'firebase/firestore';
-import { db } from "../firebaseconfig"
-
+import { db } from '../firebaseconfig';
 
 const Homepage = ({ navigation }) => {
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [posts, setPosts] = useState(null);
+  useEffect(() => {
+    const handleGetPosts = async () => {
+      const data = await getDocs(collection(db, 'posts'));
+      setPosts(data.docs);
+      setLoading(false);
+    };
 
-    useEffect(() => {
-        const handleGetPosts = async () => {
-            const data = await getDocs(collection(db, "posts"));
-            setPosts(data);
-            data.forEach((doc) => {console.log(`DOc data: ${doc.data().title}`)})
-        }
+    handleGetPosts();
+  }, []);
 
-        handleGetPosts();
-
-    }, [])
-
-    return (
-        <SafeAreaView style={styles.safeArea}>
-            {posts && posts.forEach((post) => {
-                <Text>{post.data().title}</Text>
-            }
-            )}
-                
-
-            <BottomNavBar
-                navigation={navigation}
-            />
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          {posts &&
+            posts.map((post) => (
+              <HomepagePost
+                key={post.id}
+                title={post.data().title}
+                body={post.data().body}
+                author={post.data().author}
+              />
+            ))}
+          <BottomNavBar navigation={navigation} />
+        </>
+      )}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    safeArea: {
-        flex: 1, // Ensure SafeAreaView fills the screen
-        justifyContent: 'space-between' // Pushes children to the ends of the container
-    },
-    container: {
-        flex: 1 // Take up all available space
-    },
+  safeArea: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  container: {
+    flex: 1,
+  },
 });
 
 export default Homepage;
